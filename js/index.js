0360a87d4a5333c1df6b2759aca917dbd983c9d4
wjpurdum <whitney.purdum@gmail.com>
2016-03-23 20:29:20 -0400

    Assignment 6

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
 
-	var cityAustin = true if 
-	var cityLA = true
-	var citySF = true
-	var citySydney = true
-
-	$('#city-type').submit(addEntry)
+	$('form').submit(addEntry);
 
 	function addEntry (event) {
 		event.preventDefault();
-	
-	var userInput = $('#newEntry').val();
-			if (citySF) {
-				$('body').addClass('austin');
-			}
-			if (cityLA) {
-				$('body').addClass('la')
-			}
 
-			if (cityAustin) {
-				$('body').addClass('austin')
-			}
+	var input = $('#city-type').val();
 
-			if (citySydney) = true {
-				$('body').addClass('Sydney')
-			}
+		if(input === 'San Fransisco') {
+			$('body').addClass('.sf');
+			
+		} else if (input === 'Austin') {
+			$('body').addClass('austin');
+			
+		} else if (input === 'New York') {
+			$('body').addClass('nyc');
+
+		} else if (input === 'Sydney') {
+			$('body').addClass('sydney');
+
+		} else if (input === 'Los Angeles') {
+			$('body').addClass('la');
+		}
+
+	}
+
 	
 });
 
